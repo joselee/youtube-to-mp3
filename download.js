57cd1e7db24da48a837e1d828dddd0bd07326fa4
Jose Lee <jose.lee@basware.com>
2017-11-27 11:37:12 +0200

    re-enable single video rip, sanitize filename of illegal windows filesystem chars

--- download.js
+++ download.js
@@ -2,16 +2,30 @@ const fs = require("fs");
 const path = require("path");
 const url = require("url");
 const ytpl = require("ytpl");
+const ytInfo = require("youtube-info");
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
 const argv = require("yargs")
     .option('playlist', { alias: 'p' })
-    .option('video', { alias: 'v' }).argv;
+    .option('video', { alias: 'v' })
+    .option('index', { alias: 'i' }).argv;
 
 const ffmpegPath = path.resolve(__dirname, "ffmpeg", "ffmpeg.exe");
 const outputPath = path.resolve(__dirname, "tracks");
 if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath);
 }
+
+const illegalChars = [
+    { original: `\\`, replacement: `.` },
+    { original: `/`, replacement: `.` },
+    { original: `*`, replacement: `'` },
+    { original: `:`, replacement: `-` },
+    { original: `?`, replacement: ` ` },
+    { original: `"`, replacement: `'` },
+    { original: `<`, replacement: `(` },
+    { original: `>`, replacement: `)` },
+    { original: `|`, replacement: `-` }
+];
 
 const YD = new YoutubeMp3Downloader({
     "ffmpegPath": ffmpegPath,
@@ -21,30 +35,32 @@ const YD = new YoutubeMp3Downloader({
     "progressTimeout": 5000
 });
 
-YD.on("progress", data => {});
+YD.on("progress", data => { });
 YD.on("error", error => console.log(error));
 YD.on("finished", (err, data) => console.log(`Finished (${data.videoId}) ${data.videoTitle}`));
 
-const download = (index, video) => {
-    if(index < 10) {
+const download = (video, index) => {
+    if (index && index < 10) {
         index = `0${index}`;
     }
-    YD.download(video.id, `${index} - ${video.title}.mp3`);
+
+    let title = index ? `${index} - ${video.title}.mp3` : `${video.title}.mp3`;
+    illegalChars.forEach(c => title = title.split(c.original).join(c.replacement));
+    YD.download(video.id, title);
 };
 
-if(argv.playlist) {
+if (argv.playlist) {
     const playlistId = url.parse(argv.playlist, true).query.list;
     ytpl(playlistId, (err, playlist) => {
         if (err) throw err;
 
-        for(const [index, video] of playlist.items.entries()) {
-            // console.log(index, video);
-            download(index, video);
+        for (const [index, video] of playlist.items.entries()) {
+            download(video.id, index);
         }
     });
 }
 
-// if(argv.video){
-//     const videoId = url.parse(argv.video, true).query.v;
-//     download(videoId);
-// }
+if (argv.video) {
+    const videoId = url.parse(argv.video, true).query.v;
+    ytInfo(videoId.toString()).then(v => download({ id: videoId, title: v.title }, argv.index));
+}
