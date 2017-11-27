56fdd4b79b4e24e67842c2a8f18b5ede91a2d432
Jose Lee <jose.lee@basware.com>
2017-11-27 16:52:40 +0200

    Downloader.js converted to es2015 class, code clean up

--- Downloader.js
+++ Downloader.js
@@ -1,90 +1,81 @@
-const fs = require("fs");
 const os = require("os");
-const util = require("util");
 const EventEmitter = require("events").EventEmitter;
 const ffmpeg = require("fluent-ffmpeg");
 const ytdl = require("ytdl-core");
 const async = require("async");
 const progress = require("progress-stream");
 
-function Downloader(options) {
-    let self = this;
-    self.youtubeBaseUrl = "http://www.youtube.com/watch?v=";
-    self.outputPath = options.outputPath || (os.platform() === "win32" ? "C:/Windows/Temp" : "/tmp");
-    self.queueParallelism = options.queueParallelism || 5;
-    self.illegalChars = [
-        { original: `\\`, replacement: `.` },
-        { original: `/`, replacement: `.` },
-        { original: `*`, replacement: `'` },
-        { original: `:`, replacement: `-` },
-        { original: `?`, replacement: ` ` },
-        { original: `"`, replacement: `'` },
-        { original: `<`, replacement: `(` },
-        { original: `>`, replacement: `)` },
-        { original: `|`, replacement: `-` }
-    ];
+class Downloader extends EventEmitter {
+    constructor(options) {
+        super();
+        this.outputPath = options.outputPath || (os.platform() === "win32" ? "C:/Windows/Temp" : "/tmp");
+        this.youtubeBaseUrl = "http://www.youtube.com/watch?v=";
+        this.illegalChars = [
+            { original: `\\`, replacement: `.` },
+            { original: `/`, replacement: `.` },
+            { original: `*`, replacement: `'` },
+            { original: `:`, replacement: `-` },
+            { original: `?`, replacement: ` ` },
+            { original: `"`, replacement: `'` },
+            { original: `<`, replacement: `(` },
+            { original: `>`, replacement: `)` },
+            { original: `|`, replacement: `-` }
+        ];
+        ffmpeg.setFfmpegPath(options.ffmpegPath);
 
-    ffmpeg.setFfmpegPath(options.ffmpegPath);
+        this.downloadQueue = async.queue((videoInfo, callback) => {
+            this.emit("queueSize", this.downloadQueue.running() + this.downloadQueue.length());
+            this.fetchAndEncode(videoInfo, (err, result) => callback(err, result));
+        }, (options.queueParallelism || 5));
+    }
 
-    //Async download/transcode queue
-    self.downloadQueue = async.queue(function (videoInfo, callback) {
-        self.emit("queueSize", self.downloadQueue.running() + self.downloadQueue.length());
-        self.performDownload(videoInfo, function (err, result) {
-            callback(err, result);
+
+    download(videoInfo) {
+        this.downloadQueue.push(videoInfo, (err, data) => {
+            let emitMsg = err ? "error" : "finished";
+            this.emit("queueSize", this.downloadQueue.running() + this.downloadQueue.length());
+            this.emit(emitMsg, err, data);
         });
-    }, self.queueParallelism);
-}
+    }
 
-util.inherits(Downloader, EventEmitter);
+    cleanFileName(fileName) {
+        this.illegalChars.forEach(c => fileName = fileName.split(c.original).join(c.replacement));
+        return fileName;
+    }
 
-Downloader.prototype.cleanFileName = function (fileName) {
-    let self = this;
-    self.illegalChars.forEach(c => fileName = fileName.split(c.original).join(c.replacement));
-    return fileName;
-};
+    fetchAndEncode(videoInfo, callback) {
+        const videoUrl = this.youtubeBaseUrl + videoInfo.videoId;
+        let resultObj = { videoId: videoInfo.videoId };
 
-Downloader.prototype.download = function (videoInfo) {
-    let self = this;
-    self.downloadQueue.push(videoInfo, function (err, data) {
-        let emitMsg = err ? "error" : "finished";
-        self.emit("queueSize", self.downloadQueue.running() + self.downloadQueue.length());
-        self.emit(emitMsg, err, data);
-    });
-};
+        ytdl.getInfo(videoUrl, { quality: this.youtubeVideoQuality }, (err, info) => {
+            if (err) {
+                callback(err.message, resultObj); return;
+            }
 
-Downloader.prototype.performDownload = function (videoInfo, callback) {
-    let self = this;
-    let videoUrl = self.youtubeBaseUrl + videoInfo.videoId;
-    let resultObj = { videoId: videoInfo.videoId };
-
-    ytdl.getInfo(videoUrl, { quality: self.youtubeVideoQuality }, function (err, info) {
-        if (err) {
-            callback(err.message, resultObj);
-        } else {
-            let videoTitle = self.cleanFileName(info.title);
-            let fileName = `${self.outputPath}/${videoTitle}.mp3`;
+            const videoTitle = this.cleanFileName(info.title);
+            const fileName = `${this.outputPath}/${videoTitle}.mp3`;
             resultObj.videoTitle = videoTitle;
 
             //Stream setup
-            let stream = ytdl.downloadFromInfo(info, {
+            const stream = ytdl.downloadFromInfo(info, {
                 quality: "highest",
                 requestOptions: { maxRedirects: 5 }
             });
 
             stream.on("response", (httpResponse) => {
                 //Setup of progress module
-                let str = progress({
+                const str = progress({
                     length: parseInt(httpResponse.headers["content-length"]),
                     time: 1000
                 });
 
                 //Add progress event listener
                 str.on("progress", (progress) => {
-                    self.emit("progress", { videoId: videoInfo.videoId, progress: progress })
+                    this.emit("progress", { videoId: videoInfo.videoId, progress: progress })
                 });
 
                 //Start encoding
-                let encoding = new ffmpeg({ source: stream.pipe(str)});
+                let encoding = new ffmpeg({ source: stream.pipe(str) });
                 encoding.audioBitrate(info.formats[0].audioBitrate);
                 encoding.withAudioCodec("libmp3lame");
                 encoding.toFormat("mp3");
@@ -97,7 +88,8 @@ Downloader.prototype.performDownload = function (videoInfo, callback) {
                 encoding.on("end", () => callback(null, resultObj));
                 encoding.saveToFile(fileName);
             });
-        }
-    });
-};
+        });
+    }
+}
+
 module.exports = Downloader;
